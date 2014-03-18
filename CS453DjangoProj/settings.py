aefa8f9cb514fc1e61946d2cbb55f685cb5b5f47
Alisha Hayman <alishaagyt@gmail.com>
2014-03-18 19:51:49 -0400

    First changes to settings and models (cart and returns)

--- CS453DjangoProj/settings.py
+++ CS453DjangoProj/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookrental',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +58,12 @@ WSGI_APPLICATION = 'CS453DjangoProj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'example1',
+        'USER': 'vagrant',
+        'PASSWORD': 'vagrant',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
---
+++ bookrental/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models/tables here.
+# ! Foreign keys go BELOW tables they reference !
+
+class Cart(models.Model):
+     username = models.CharField(max_length=15, primary_key=True) # foreign key to login
+     isbn = models.CharField(max_length=17) # foreign key to book
+     quantity = models.IntegerField(default=0)
+     price = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Returns(models.Model):
+     username = models.CharField(max_length=15, primary_key=True) # foreign key to login
+     isbn = models.CharField(max_length=17) # foreign key to book
+     returndate = models.DateField(auto_now=False, auto_now_add=False)
+    #poll = models.ForeignKey(Poll)
