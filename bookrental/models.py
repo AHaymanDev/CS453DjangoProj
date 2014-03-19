eb305810a8eea840ffcc271f98756030a7737deb
AHaymanDev <alishagyt@gmail.com>
2014-03-19 18:50:26 -0400

    Updated login and position in models.py.

--- bookrental/models.py
+++ bookrental/models.py
@@ -3,9 +3,22 @@ from django.db import models
 # Create your models/tables here.
 # ! Foreign keys go BELOW tables they reference !
 
+class Login(models.Model):
+     username = models.CharField(max_length=15, primary_key=True)
+     password = models.CharField(max_length=15)
+     name = models.CharField(max_length=15)
+     email = models.CharField(max_length=40)
+     phone = models.CharField(max_length=10)
+     def __unicode__(self):
+          return self.username, self.password
+     def user_info(self):
+          return self.name, self.email, self.phone
+
 class Cart(models.Model):
      username = models.CharField(max_length=15, primary_key=True) # foreign key to login
+     #poll = models.ForeignKey(Poll)
      isbn = models.CharField(max_length=17) # foreign key to book
+     #poll = models.ForeignKey(Poll)
      quantity = models.IntegerField(default=0)
      price = models.DecimalField(max_digits=5, decimal_places=2)
      def __unicode__(self):
@@ -13,20 +26,11 @@ class Cart(models.Model):
 
 class Returns(models.Model):
      username = models.CharField(max_length=15, primary_key=True) # foreign key to login
-     isbn = models.CharField(max_length=17) # foreign key to book
-     returndate = models.DateField(auto_now=False, auto_now_add=False)
      #poll = models.ForeignKey(Poll)
+     isbn = models.CharField(max_length=17) # foreign key to book
+     #poll = models.ForeignKey(Poll)
+     returndate = models.DateField(auto_now=False, auto_now_add=False)
      def __unicode__(self):
      	 return self.username, self.isbn, self.returndate
      	 
-class Login(models.Model):
-     def __unicode__(self):
-          return self.username, self.password
-     def user_info(self):
-          return self.name, self.email, self.phone
-          
-     username = models.CharField(max_length=15, primary_key=True)
-     password = models.CharField(max_length=15)
-     name = models.CharField(max_length=15)
-     email = models.CharField(max_length=40)
-     phone = models.CharField(max_length=10)
+
