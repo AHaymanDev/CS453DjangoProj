a89989ce84c399fee86d6b617ec6601c1cb21057
Alisha Hayman <alishaagyt@gmail.com>
2014-03-18 21:08:03 -0400

    Added html pages in templates and updated views.py to reflect this.

--- bookrental/views.py
+++ bookrental/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 
 # Create your views here.
 
-def cart(request):
-     return HttpResponse("You are at the bookrental cart.")
+def book(request):
+     return render_to_response('bookrental/Books.html')
 
+def checkout(request):
+     return render_to_response('bookrental/Checkout.html')
+
+def info(request):
+     return render_to_response('bookrental/InfoPage.html')
+
+def login(request):
+     return render_to_response('bookrental/Login.html')
+
+def return_confirm(request):
+     return render_to_response('bookrental/ReturnConfirm.html')
+
+def returns(request):
+     return render_to_response('bookrental/Returns.html')
+
+def warning(request):
+     return render_to_response('bookrental/Warningg.html')
+
+def cart(request):
+     return render_to_response('bookrental/YourCart.html')
+
+def category(request):
+     return render_to_response('bookrental/category.html')
