52bcdd03a4f40abb107faebe4a7d4d878869ca6d
jcmalott1 <jcmalott1@catamount.wcu.edu>
2014-03-20 19:50:12 -0700

    admin.py edite
    add function from sign up to admin site

--- bookrental/admin.py
+++ bookrental/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+# Allows one to add function from the models/SignUp
+# to the admin site
+from models import SignUp
+
+class SignUpAdmin(admin.ModelAdmin):
+    class Meta:
+        model = SignUp
+        
+admin.site.register(SignUp, SignUpAdmin)
