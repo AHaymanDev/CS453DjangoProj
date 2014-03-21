a67d8832f79d86bd4eeb7f161fe93b1e5290fc08
AHaymanDev <alishagyt@gmail.com>
2014-03-21 11:26:42 -0400

    Added foreign keys.

--- bookrental/models.py
+++ bookrental/models.py
@@ -28,20 +28,16 @@ class Categories(models.Model):
           return self.category
 
 class Cart(models.Model):
-     username = models.CharField(max_length=15, primary_key=True) # foreign key to login
-     #poll = models.ForeignKey(Poll)
-     isbn = models.CharField(max_length=17) # foreign key to book
-     #poll = models.ForeignKey(Poll)
+     username = models.ForeignKey(Login) #models.CharField(max_length=15, primary_key=True) # foreign key to login
+     isbn = models.ForeignKey(Book) #models.CharField(max_length=17) # foreign key to book
      quantity = models.IntegerField(default=0)
      price = models.DecimalField(max_digits=5, decimal_places=2)
      def __unicode__(self):
          return self.username, self.isbn, self.quantity, self.price
 
 class Returns(models.Model):
-     username = models.CharField(max_length=15, primary_key=True) # foreign key to login
-     #poll = models.ForeignKey(Poll)
-     isbn = models.CharField(max_length=17) # foreign key to book
-     #poll = models.ForeignKey(Poll)
+     username = models.ForeignKey(Login) #models.CharField(max_length=15, primary_key=True) # foreign key to login
+     isbn = models.ForeignKey(Book) #models.CharField(max_length=17) # foreign key to book
      returndate = models.DateField(auto_now=False, auto_now_add=False)
      def __unicode__(self):
      	 return self.username, self.isbn, self.returndate
