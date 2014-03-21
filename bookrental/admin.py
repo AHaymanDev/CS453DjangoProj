03384d2afa1b71a257df65dcc04e37c19cfd5845
jcmalott1 <jcmalott1@catamount.wcu.edu>
2014-03-20 20:16:34 -0700

    Update admin.py

--- bookrental/admin.py
+++ bookrental/admin.py
@@ -1,12 +1,4 @@
 from django.contrib import admin
 
 # Register your models here.
-# Allows one to add function from the models/SignUp
-# to the admin site
-from models import SignUp
 
-class SignUpAdmin(admin.ModelAdmin):
-    class Meta:
-        model = SignUp
-        
-admin.site.register(SignUp, SignUpAdmin)
