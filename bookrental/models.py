b2e42cc494b388f6a68d3bb7c09a02e661078a46
jcmalott1 <jcmalott1@catamount.wcu.edu>
2014-03-21 11:55:48 -0400

    Update models.py

--- bookrental/models.py
+++ bookrental/models.py
@@ -12,6 +12,7 @@ class Login(models.Model):
      name = models.CharField(max_length=15)
      email = models.CharField(max_length=40)
      phone = models.CharField(max_length=10)
+     #cart = models.ManyToManyField(Cart)
      def __unicode__(self):
           return self.username, self.password
      def user_info(self):
