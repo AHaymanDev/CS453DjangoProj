d9eadf9dcc6a066dfeb2f485a2ad68230c65bc08
Alisha Hayman <alishaagyt@gmail.com>
2014-04-19 20:04:39 -0400

    Updating user registration. Afterwards, test for successful login.

--- bookrental/forms.py
+++ bookrental/forms.py
@@ -11,18 +11,14 @@ class UserCreateForm(UserCreationForm):
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.EmailField()
-    phone = forms.CharField()
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'password2')
+        fields = ('username', 'password', 'password2', 'email')
 
     def save(self, commit=True):
         user = super(UserCreateForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
-        user.first_name = self.cleaned['first_name']
-        user.last_name = self.cleaned['last_name']
-        user.phone = self.cleaned_data['phone']
 
         if commit:
             user.save()
