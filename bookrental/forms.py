7d4ee23097aed42bbc92a3c151fe813da59f2cc3
Alisha Hayman <alishaagyt@gmail.com>
2014-04-21 20:08:42 -0400

    Working on categories and books.

--- bookrental/forms.py
+++ bookrental/forms.py
@@ -19,6 +19,8 @@ class UserCreateForm(UserCreationForm):
     def save(self, commit=True):
         user = super(UserCreateForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
 
         if commit:
             user.save()
